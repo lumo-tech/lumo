9c89d92d956f36016e8758ce9ee618263c2c13d0
sailist <sailist@outlook.com>
2023-02-01 21:53:50 +0800

    publish test

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ print(find_packages('src'))
 
 setup(
     name='lumo',
-    version="0.13.0",
+    version="0.14.0",
     description='library to manage your pytorch experiments.',
     url='https://github.com/sailist/lumo',
     author='sailist',
--- src/lumo/__init__.py
+++ src/lumo/__init__.py
@@ -1,7 +1,7 @@
 """
 
 """
-__version__ = "0.13.0"
+__version__ = "0.14.0"
 
 from .core import Params, ParamsType, MetricType, Meter, Record, TrainStage, BaseParams
 from .data import DataLoader, DataModule, DatasetBuilder, LumoDataLoader, CollateBase, DataLoaderSide
