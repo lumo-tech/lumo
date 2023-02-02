dff621bf6e8526a409efe7b789b6c55afca07f9c
sailist <sailist@outlook.com>
2023-02-02 11:06:07 +0800

    Ensure workflow

--- setup.py
+++ setup.py
@@ -8,25 +8,26 @@ def extract_version():
         open('src/lumo/__init__.py', 'r', encoding='utf-8').read()).group(1)
 
 
-setup(
-    name='lumo',
-    version=extract_version(),
-    description='library to manage your pytorch experiments.',
-    long_description_content_type='text/markdown',
-    url='https://github.com/pytorch-lumo/lumo',
-    author='sailist',
-    author_email='sailist@outlook.com',
-    license='Apache License 2.0',
-    include_package_data=True,
-    classifiers=[
-        'Programming Language :: Python :: 3.9',
-    ],
-    package_dir={"": "src"},
-    keywords='lumo',
-    packages=find_packages('src'),
-    entry_points={
-        'console_scripts': [
-            'lumo = lumo.cli.cli:main'
-        ]
-    },
-)
+if __name__ == '__main__':
+    setup(
+        name='lumo',
+        version=extract_version(),
+        description='library to manage your pytorch experiments.',
+        long_description_content_type='text/markdown',
+        url='https://github.com/pytorch-lumo/lumo',
+        author='sailist',
+        author_email='sailist@outlook.com',
+        license='Apache License 2.0',
+        include_package_data=True,
+        classifiers=[
+            'Programming Language :: Python :: 3.9',
+        ],
+        package_dir={"": "src"},
+        keywords='lumo',
+        packages=find_packages('src'),
+        entry_points={
+            'console_scripts': [
+                'lumo = lumo.cli.cli:main'
+            ]
+        },
+    )
