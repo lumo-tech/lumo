81ca6bbc29e08bb4b1d43bf453b4c9e365c1f705
sailist <sailist@outlook.com>
2023-02-14 10:35:13 +0800

    change version

--- src/lumo/__init__.py
+++ src/lumo/__init__.py
@@ -1,7 +1,7 @@
 """
 
 """
-__version__ = "0.14.2"
+__version__ = "0.14.3"
 
 from .core import Params, ParamsType, MetricType, Meter, Record, TrainStage, BaseParams
 from .data import DataLoader, DataModule, DatasetBuilder, LumoDataLoader, CollateBase, DataLoaderSide
