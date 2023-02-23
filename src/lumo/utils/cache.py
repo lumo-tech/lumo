8ff7006900837fb372eaee0751af5660410f2b08
sailist <sailist@outlook.com>
2023-02-23 20:39:17 +0800

    manully implement cache for lower version

--- src/lumo/utils/cache.py
+++ src/lumo/utils/cache.py
@@ -1,6 +1,6 @@
 from functools import lru_cache
 
 
-def cache(user_function, /):
+def cache(user_function):
     """Simple lightweight unbounded cache.  Sometimes called "memoize"."""
     return lru_cache(maxsize=None)(user_function)
