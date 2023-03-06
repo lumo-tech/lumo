ad5a70571af78328a6b9513b8b8b8ea2a1257c34
sailist <sailist@outlook.com>
2023-03-06 19:20:43 +0800

    Fix problem of update in Params

--- src/lumo/core/params.py
+++ src/lumo/core/params.py
@@ -20,123 +20,6 @@ from .raises import BoundCheckError
 __all__ = ['BaseParams', 'Params', 'ParamsType']
 
 
-def safe_update_dict(src: dict, kwargs: dict, assert_type=False):
-    """
-   Updates the source dictionary with the key-value pairs from the kwargs dictionary in a safe manner.
-
-   This function iterates over the items in the kwargs dictionary and updates the corresponding items in the
-   source dictionary, making sure that the types of the values being updated match the types of the values
-   already in the source dictionary.
-
-   Args:
-       src (dict): The dictionary to update.
-       kwargs (dict): The dictionary containing the new key-value pairs to add to the source dictionary.
-       assert_type (bool): A flag indicating whether to check that the types of the values being updated match
-           the types of the values already in the source dictionary. Defaults to True.
-
-   Returns:
-       dict: The updated source dictionary.
-   """
-    for ks, v in walk_dict(kwargs):
-        try:
-            old_v = get_item_iterative(src, ks)
-            if old_v is None or isinstance(old_v, type(v)) or not assert_type:
-                set_item_iterative(src, ks, v)
-            else:
-                raise TypeError(ks, type(old_v), type(v))
-        except KeyError:
-            set_item_iterative(src, ks, v)
-    return src
-
-
-def walk_dict(dic: dict, root=None):
-    """
-    Recursively walks through a dictionary and yields keys and values in a flattened format.
-
-    Args:
-    - dic (dict): The dictionary to be walked through.
-    - root (list): The root keys to be used in the resulting flattened format. Defaults to None.
-
-    Yields:
-    - A tuple containing a list of keys and a value. The list of keys is composed of the root keys and the current keys in the dictionary, split by '.' if there are any. The value is the corresponding value in the dictionary.
-
-    Example:
-        ```python
-        d = {'a': {'b': 1, 'c': {'d': 2}}, 'e': 3}
-        for k, v in walk_dict(d):
-            print(k, v)
-        # Output:
-        # (['a', 'b'], 1)
-        # (['a', 'c', 'd'], 2)
-        # (['e'], 3)
-        ```
-    """
-    if root is None:
-        root = []
-    for k, v in dic.items():
-        if isinstance(v, dict):
-            yield from walk_dict(v, [*root, *k.split('.')])
-        else:
-            yield [*root, *k.split('.')], v
-
-
-def set_item_iterative(dic: dict, keys: List[str], value):
-    """
-    Sets the value of a nested key in a dictionary using an iterative approach.
-
-    Args:
-        dic (dict): The dictionary to update.
-        keys (List[str]): A list of keys representing the path to the nested key in the dictionary.
-        value: The value to set for the nested key.
-
-    Raises:
-        ValueError: If a key in the path exists in the dictionary but the corresponding value is not a dictionary.
-
-    """
-    if len(keys) == 1:
-        if isinstance(value, MutableMapping):
-            for ks, v in walk_dict(value):
-                set_item_iterative(dic, [*keys, *ks], v)
-        else:
-            dic.__setitem__(keys[0], value)
-    else:
-        try:
-            nex = dic.__getitem__(keys[0])
-            if not isinstance(nex, MutableMapping):
-                raise ValueError(keys[0], nex)
-            # dict.__setitem__(dic, keys[0], nex)
-        except KeyError:
-            nex = BaseParams()
-            dic.__setitem__(keys[0], nex)
-
-        set_item_iterative(nex, keys[1:], value)
-
-
-def get_item_iterative(dic: dict, keys: List[str]):
-    """
-    Gets the value of a nested key in a dictionary using an iterative approach.
-
-    Args:
-        dic (dict): The dictionary to retrieve the value from.
-        keys (List[str]): A list of keys representing the path to the nested key in the dictionary.
-
-    Raises:
-        KeyError: If the nested key does not exist in the dictionary.
-
-    Returns:
-        The value of the nested key in the dictionary.
-
-    """
-    if len(keys) == 1:
-        return dic.__getitem__(keys[0])
-    else:
-        nex = dic.__getitem__(keys[0])
-        if isinstance(nex, dict):
-            return get_item_iterative(nex, keys[1:])
-        else:
-            raise KeyError(keys)
-
-
 class Arange:
     """A class representing a range of numeric values with a default, left and right boundaries.
 
@@ -437,7 +320,7 @@ class BaseParams(DictConfig):
         """
         safe_update_dict(self, dic, assert_type=assert_type)
 
-    def from_dict(self, dic: dict):
+    def from_dict(self, dic: MutableMapping):
         """
         Update the config object from a dictionary.
 
@@ -506,17 +389,19 @@ class BaseParams(DictConfig):
                 print(self)
                 exit()
                 return
-
             config = kwargs.get('config')
             if config is None:
                 config = kwargs.get('c')
             if config is not None and isinstance(config, str) and os.path.exists(config):
-                self.from_yaml(config)
+                if config.endswith('yaml'):
+                    self.from_yaml(config)
+                elif config.endswith('json'):
+                    self.from_json(config)
 
-            dic = {}
+            dic = BaseParams()
             for k, v in kwargs.items():
                 set_item_iterative(dic, k.split('.'), v)
-            self.safe_update(dic)
+            self.safe_update(dic.to_dict())
 
         fire.Fire(func, command=argv)
         return self
@@ -626,3 +511,120 @@ class Params(BaseParams):
 
 
 ParamsType = NewType('ParamsType', Params)
+
+
+def safe_update_dict(src: BaseParams, kwargs: dict, assert_type=False):
+    """
+   Updates the source dictionary with the key-value pairs from the kwargs dictionary in a safe manner.
+
+   This function iterates over the items in the kwargs dictionary and updates the corresponding items in the
+   source dictionary, making sure that the types of the values being updated match the types of the values
+   already in the source dictionary.
+
+   Args:
+       src (dict): The dictionary to update.
+       kwargs (dict): The dictionary containing the new key-value pairs to add to the source dictionary.
+       assert_type (bool): A flag indicating whether to check that the types of the values being updated match
+           the types of the values already in the source dictionary. Defaults to True.
+
+   Returns:
+       dict: The updated source dictionary.
+   """
+    for ks, v in walk_dict(kwargs):
+        try:
+            old_v = get_item_iterative(src, ks)
+            if old_v is None or isinstance(old_v, type(v)) or not assert_type:
+                set_item_iterative(src, ks, v)
+            else:
+                raise TypeError(ks, type(old_v), type(v))
+        except KeyError:
+            set_item_iterative(src, ks, v)
+    return src
+
+
+def walk_dict(dic: MutableMapping, root=None):
+    """
+    Recursively walks through a dictionary and yields keys and values in a flattened format.
+
+    Args:
+    - dic (dict): The dictionary to be walked through.
+    - root (list): The root keys to be used in the resulting flattened format. Defaults to None.
+
+    Yields:
+    - A tuple containing a list of keys and a value. The list of keys is composed of the root keys and the current keys in the dictionary, split by '.' if there are any. The value is the corresponding value in the dictionary.
+
+    Example:
+        ```python
+        d = {'a': {'b': 1, 'c': {'d': 2}}, 'e': 3}
+        for k, v in walk_dict(d):
+            print(k, v)
+        # Output:
+        # (['a', 'b'], 1)
+        # (['a', 'c', 'd'], 2)
+        # (['e'], 3)
+        ```
+    """
+    if root is None:
+        root = []
+    for k, v in dic.items():
+        if isinstance(v, dict):
+            yield from walk_dict(v, [*root, *k.split('.')])
+        else:
+            yield [*root, *k.split('.')], v
+
+
+def set_item_iterative(dic: BaseParams, keys: List[str], value):
+    """
+    Sets the value of a nested key in a dictionary using an iterative approach.
+
+    Args:
+        dic (dict): The dictionary to update.
+        keys (List[str]): A list of keys representing the path to the nested key in the dictionary.
+        value: The value to set for the nested key.
+
+    Raises:
+        ValueError: If a key in the path exists in the dictionary but the corresponding value is not a dictionary.
+
+    """
+    if len(keys) == 1:
+        if isinstance(value, MutableMapping):
+            for ks, v in walk_dict(value):
+                set_item_iterative(dic, [*keys, *ks], v)
+        else:
+            dic.__setitem__(keys[0], value)
+    else:
+        try:
+            nex = dic.__getitem__(keys[0])
+            if not isinstance(nex, MutableMapping):
+                raise ValueError(keys[0], nex)
+            # dict.__setitem__(dic, keys[0], nex)
+        except KeyError:
+            nex = BaseParams()
+            dic.__setitem__(keys[0], nex)
+
+        set_item_iterative(nex, keys[1:], value)
+
+
+def get_item_iterative(dic: MutableMapping, keys: List[str]):
+    """
+    Gets the value of a nested key in a dictionary using an iterative approach.
+
+    Args:
+        dic (dict): The dictionary to retrieve the value from.
+        keys (List[str]): A list of keys representing the path to the nested key in the dictionary.
+
+    Raises:
+        KeyError: If the nested key does not exist in the dictionary.
+
+    Returns:
+        The value of the nested key in the dictionary.
+
+    """
+    if len(keys) == 1:
+        return dic.__getitem__(keys[0])
+    else:
+        nex = dic.__getitem__(keys[0])
+        if isinstance(nex, MutableMapping):
+            return get_item_iterative(nex, keys[1:])
+        else:
+            raise KeyError(keys)
