cc14d85e3cf748d58e8bcb55430e81684211d0a8
sailist <sailist@outlook.com>
2023-03-13 14:28:22 +0800

    Add deprecated warning

--- src/lumo/analyse/__init__.py
+++ src/lumo/analyse/__init__.py
@@ -1,2 +1,5 @@
+import warnings
+
 from .collect import collect_table_rows, flatten_dict, flatten_params, flatten_metric
 from .condition import C, filter_by_condition
+warnings.warn("lumo.analyse has been deprecated and will be removed soon, please use lumo.exp.Watcher instead.")
