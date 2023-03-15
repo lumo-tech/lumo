b5f7f19838f814e6fafe7c9bd2afb57a62de8afc
sailist <sailist@outlook.com>
2023-03-15 21:10:12 +0800

    import

--- src/lumo/__init__.py
+++ src/lumo/__init__.py
@@ -4,8 +4,9 @@
 __version__ = "0.15.0"
 
 from .core import Params, ParamsType, MetricType, Meter, Record, TrainStage, BaseParams
+from .proc import glob
 
 from .data import DataLoader, DataModule, DatasetBuilder, LumoDataLoader, CollateBase, DataLoaderSide
-from .exp import SimpleExperiment, Experiment
+from .exp import SimpleExperiment, Experiment, Metric, Watcher, C
 from .trainer import Trainer, TrainerParams, callbacks, RndManager
 from .utils import Logger
