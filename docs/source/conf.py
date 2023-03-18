e310866c3891e1cd3782ab69aeeaef1324e783ce
sailist <sailist@outlook.com>
2023-03-18 16:14:25 +0800

    Document: update

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,36 +1,62 @@
-# Configuration file for the Sphinx documentation builder.
-#
 # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
-import recommonmark
-from recommonmark.transform import AutoStructify
-from recommonmark.parser import CommonMarkParser
-
-
-def setup(app):
-    # app.add_config_value('recommonmark_config', {
-    #     'auto_toc_tree_section': 'Contents',
-    # }, True)
-    app.add_transform(AutoStructify)
-
+import extract_version
 
 project = 'lumo'
 copyright = '2023, sailist'
 author = 'sailist'
-release = '1.0.0'
+release = extract_version()
+
+# -- Path setup --------------------------------------------------------------
+
+# If extensions (or modules to document with autodoc) are in another directory,
+# add these directories to sys.path here. If the directory is relative to the
+# documentation root, use os.path.abspath to make it absolute, like shown here.
+#
+import os
+import sys
+
+sys.path.insert(0, os.path.abspath('../../src/'))
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
+from recommonmark.transform import AutoStructify
+from recommonmark.parser import CommonMarkParser
+import commonmark
+
 extensions = [
-    'sphinx.ext.autodoc',
     "myst_parser",
+    # "autodoc2",
+    "sphinx_rtd_theme", # pip install sphinx_rtd_theme
+    'sphinx.ext.napoleon',
+    'sphinx.ext.autodoc',
 
 ]
 
+
+def docstring(app, what, name, obj, options, lines):
+    md = '\n'.join(lines)
+    ast = commonmark.Parser().parse(md)
+    rst = commonmark.ReStructuredTextRenderer().render(ast)
+    lines.clear()
+    lines += rst.splitlines()
+
+
+def setup(app):
+    app.add_transform(AutoStructify)
+    app.add_config_value('recommonmark_config', {
+        'auto_toc_tree_section': 'Contents',
+    }, True)
+    app.connect('autodoc-process-docstring', docstring)
+
+
+autodoc2_packages = [
+    "../../src/library",
+]
 myst_enable_extensions = [
     "deflist",
 ]
@@ -41,7 +67,7 @@ exclude_patterns = []
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
 #
 # source_suffix = {
@@ -50,9 +76,17 @@ html_static_path = ['_static']
 #     '.md': 'markdown',
 # }
 #
+commonmark_suffixes = ['.rst']
 
 source_parsers = {
     '.md': CommonMarkParser,
 }
 
 source_suffix = ['.rst', '.md']
+
+autodoc2_docstring_parser_regexes = [
+    # this will render all docstrings as Markdown
+    (r".*", "myst"),
+    # this will render select docstrings as Markdown
+    (r"autodoc2\..*", "myst"),
+]
--- docs/source/index.rst
+++ docs/source/index.rst
@@ -1,5 +1,5 @@
 .. lumo documentation master file, created by
-   sphinx-quickstart on Fri Mar 17 21:42:08 2023.
+   sphinx-quickstart on Sat Mar 18 14:41:26 2023.
    You can adapt this file completely to your liking, but it should at least
    contain the root `toctree` directive.
 
@@ -8,9 +8,9 @@ Welcome to lumo's documentation!
 
 .. toctree::
    :maxdepth: 2
-   :caption: Contents:
+   :caption: Contents
 
-
+   ../tutorial/getting_start.md
 
 Indices and tables
 ==================
