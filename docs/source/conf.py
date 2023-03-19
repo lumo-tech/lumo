ea148828f9bb7051a0d55a463bbc82c0538dbe44
sailist <sailist@outlook.com>
2023-03-19 22:05:02 +0800

    workflow: build docs by sphinx-multiversion

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -6,6 +6,8 @@
 import re
 from pathlib import Path
 
+from sphinx.application import Sphinx
+
 version_fn = Path(__file__).parent.parent.parent.joinpath('src/lumo/__init__.py').as_posix()
 
 
@@ -41,7 +43,9 @@ import commonmark
 extensions = [
     "myst_parser",
     # "autodoc2",
-    "sphinx_rtd_theme",  # pip install sphinx_rtd_theme
+
+    "sphinx_multiversion",
+    # "custom_rtd_theme",  # pip install custom_rtd_theme
     'sphinx.ext.napoleon',
     'sphinx.ext.autodoc',
 
@@ -56,12 +60,30 @@ def docstring(app, what, name, obj, options, lines):
     lines += rst.splitlines()
 
 
-def setup(app):
+def html_page_context(app, pagename, templatename, context, doctree):
+    # versioninfo = VersionInfo(
+    #     app, context, app.config.smv_metadata, app.config.smv_current_version
+    # )
+    context["READTHEDOCS"] = True
+    context["theme_display_version"] = True
+    # context["vhasdoc"] = versioninfo.vhasdoc
+    # context["vpathto"] = versioninfo.vpathto
+    #
+    context["current_version"] = extract_version()
+    # context["latest_version"] = versioninfo[app.config.smv_latest_version]
+    # context["html_theme"] = app.config.html_theme
+    pass
+
+
+def setup(app: Sphinx):
     app.add_transform(AutoStructify)
     app.add_config_value('recommonmark_config', {
         'auto_toc_tree_section': 'Contents',
     }, True)
     app.connect('autodoc-process-docstring', docstring)
+    app.connect("html-page-context", html_page_context)
+
+    app.add_html_theme('custom_rtd_theme', Path(__file__).parent.joinpath('custom_rtd_theme/').as_posix())
 
 
 autodoc2_packages = [
@@ -77,7 +99,29 @@ exclude_patterns = []
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'sphinx_rtd_theme'
+
+# html_theme = Path(__file__).parent.joinpath('custom_rtd_theme/').as_posix()
+html_theme = "custom_rtd_theme"
+# html_theme = "sphinx_rtd_theme"
+# html_theme = "alabaster"
+html_theme_options = {
+    # 'github_url': 'https://github.com/pytorch-lumo/lumo',
+    # 'analytics_id': 'G-XXXXXXXXXX',  #  Provided by Google in your dashboard
+    # 'analytics_anonymize_ip': False,
+    # 'logo_only': False,
+    # 'display_version': True,
+    # 'prev_next_buttons_location': 'bottom',
+    # 'style_external_links': False,
+    # 'vcs_pageview_mode': '',
+    # 'style_nav_header_background': 'white',
+    # # Toc options
+    # 'collapse_navigation': True,
+    # 'sticky_navigation': True,
+    # 'navigation_depth': 4,
+    # 'includehidden': True,
+    # 'titles_only': False
+}
+
 html_static_path = ['_static']
 #
 # source_suffix = {
@@ -94,9 +138,12 @@ source_parsers = {
 
 source_suffix = ['.rst', '.md']
 
-autodoc2_docstring_parser_regexes = [
-    # this will render all docstrings as Markdown
-    (r".*", "myst"),
-    # this will render select docstrings as Markdown
-    (r"autodoc2\..*", "myst"),
-]
+# html_sidebars = {
+#     '**': [
+#         # "my_side.html",
+#         # 'versions.html',
+#     ],
+# }
+smv_outputdir_format = f'v{extract_version()}'
+# smv_tag_whitelist = r'^.*$'  # Include all tags
+smv_tag_whitelist = r'^v\d+\.\d+.\d+.\d+$'  # Include tags like "v2.1"
